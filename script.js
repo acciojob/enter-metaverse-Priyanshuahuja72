32c729442f9eb558eecfc31384e8eba41f48b222
Priyanshu Ahuja <85331994+Priyanshuahuja72@users.noreply.github.com>
2023-04-12 22:17:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let btn = document.getElementById("enterBtn");
+btn.addEventListener("click" , ()=> {
+   let heading = document.createElement("h1");
+   heading.innerText = "Entered Metaverse";
+   document.body.appendChild(heading);
+   let paratag = document.getElementById("status");
+   paratag.remove();
+})
